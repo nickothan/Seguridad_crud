71449ce680c185c864ae31d4c461eee19e8639e5
Sergio Ramirez <s1nicolasrg@gmail.com>
2019-05-24 13:32:51 -0500

    validacion de campos y validacion por expreciones regulares para el input de usuario

--- views/js/validarRegistro.js
+++ views/js/validarRegistro.js
@@ -9,5 +9,20 @@ function validarRegistro(){
     console.log("password" , password);
     var email = document.querySelector('#emailRegistro').value;
     console.log("email " , email);
-    return false;
+    /*VALIDAR USUARIO*/
+    if (usuario != ""){
+        /* Validar espacio de usuario Registro*/
+        var caracteres = usuario.length;
+        if(caracteres > 6){
+            document.querySelector("label[for='usuarioRegistro']").innerHTML +=" <br> Escriba menos de 6 caracteres";
+            return false;
+        }
+        /*VALIDAR CARACTERES ESPECIALES DE usuario*/
+        var expresion = /  ^[a-zA-Z0-9]*$ /;
+        if (!expresion.test(usuario)){
+            document.querySelector("label[for='usuarioRegistro']").innerHTML +=" <br> por favor no escriba caracteres especiales";
+            return false;
+        }
+    }
+    return true;
 }
